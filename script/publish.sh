b1c689bf545721a834ebde55d5ef660e630aa186
Steven Miers <steven.miers@gmail.com>
2024-03-28 16:02:48 -0500

    Add reminder to tag.

--- script/publish.sh
+++ script/publish.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 poetry publish --build -u __token__ -p $PYPI_TOKEN
+
+echo "Remember to tag:"
+echo 'git tag -a v0.1.1 -m "Version 0.1.1 - renamed project for compatibility."'
+echo 'git push origin v.0.1.1'
